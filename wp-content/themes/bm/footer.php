c51c73faf2c51b28936c8ad0fd4d38a264b7ebd2
Joseph West <joe@incitenewbusiness.co.uk>
2026-02-05 09:42:20 +0000

    remove cookie banner

--- wp-content/themes/bm/footer.php
+++ wp-content/themes/bm/footer.php
@@ -248,11 +248,11 @@
   </script>
 <?php endif; ?>
 
-<section role="region" aria-label="Cookie preferences">
+<!--<section role="region" aria-label="Cookie preferences">
   <div id="cookiebtn">
     <a id="cookiechange" role="button" tabindex="0" onclick="Cookiebot.show();" onkeydown="if(event.key === 'Enter' || event.key === ' ') { Cookiebot.show(); event.preventDefault(); }">Change your cookie consent</a>
   </div>
-</section>
+</section>-->
 
 <style>
 body.home h1.header {

5e739dec6d864a91fa3e350ea6befcd49a333538
Joseph West <joe@incitenewbusiness.co.uk>
2026-02-17 16:39:27 +0000

    New social icons on blog

--- wp-content/themes/bm/modules/parts/single-post/social-share.php
+++ wp-content/themes/bm/modules/parts/single-post/social-share.php
@@ -19,26 +19,26 @@ $title     = urlencode(get_the_title());
              target="_blank"
              rel="noopener"
              aria-label="Share on Facebook">
-            <img src="/wp-content/uploads/2020/09/fb-icon.png" alt="">
+            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/theme/img/fb-purple-icon.svg" alt="Share on Facebook">
           </a>
 
           <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $permalink; ?>&title=<?php echo $title; ?>"
              target="_blank"
              rel="noopener"
              aria-label="Share on LinkedIn">
-            <img src="/wp-content/uploads/2020/09/linkedin-icon.png" alt="">
+            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/theme/img/linkedin-purple-icon.svg" alt="Share on LinkedIn">
           </a>
 
-          <a href="https://twitter.com/intent/tweet?url=<?php echo $permalink; ?>&text=<?php echo $title; ?>"
+          <!--<a href="https://twitter.com/intent/tweet?url=<?php echo $permalink; ?>&text=<?php echo $title; ?>"
              target="_blank"
              rel="noopener"
              aria-label="Share on X">
             <img src="/wp-content/uploads/2020/09/twitter-icon.png" alt="">
-          </a>
+          </a>-->
 
           <a href="mailto:?subject=<?php echo $title; ?>&body=<?php echo $permalink; ?>"
              aria-label="Share by email">
-            <img src="/wp-content/uploads/2020/09/mail-icon1.png" alt="">
+            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/theme/img/email-purple-icon.svg" alt="Share via Email">
           </a>
 
         </div>

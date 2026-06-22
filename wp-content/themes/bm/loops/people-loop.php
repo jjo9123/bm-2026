c04180e101f1515aec6e10b42cddb6831467d100
Joseph West <joe@incitenewbusiness.co.uk>
2026-06-22 16:52:10 +0100

    Fix people loop

--- wp-content/themes/bm/loops/people-loop.php
+++ wp-content/themes/bm/loops/people-loop.php
@@ -10,57 +10,70 @@
 <div id="results">
   <div class="row staff cards experts-cards">
     <?php
-$char = isset($_GET['query']) ? sanitize_text_field(wp_unslash($_GET['query'])) : '';
+      $char = isset($_GET['query']) ? sanitize_text_field(wp_unslash($_GET['query'])) : '';
 
-$paged = get_query_var('paged') ? (int) get_query_var('paged') : 1;
+      $paged = get_query_var('paged') ? (int) get_query_var('paged') : 1;
 
-$args = array(
-  'post_type'      => 'people',
-  'posts_per_page' => 12,
-  'paged'          => $paged,
-  'orderby'        => 'meta_value',
-  'meta_key'       => 'contact_details_last_name',
-  'order'          => 'asc',
-);
+      $args = array(
+        'post_type'        => 'people',
+        'posts_per_page'   => 12,
+        'paged'            => $paged,
+        'orderby'          => 'meta_value',
+        'meta_key'         => 'contact_details_last_name',
+        'order'            => 'asc',
+        'search_filter_id' => 680,
+      );
 
-if ( $char !== '' ) {
-  $args['meta_query'] = array(
-    array(
-      'key'     => 'contact_details_last_name',
-      'value'   => $char,
-      'compare' => 'LIKE',
-    )
-  );
+      if ( $char !== '' ) {
+        $args['starts_with'] = $char;
 
-  // if you're using Search & Filter Pro (your filter id suggests you are)
-  $args['search_filter_id'] = 680;
+        $args['meta_query'] = array(
+          array(
+            'key'     => 'contact_details_last_name',
+            'value'   => $char,
+            'compare' => 'LIKE',
+          )
+        );
+      }
 
-  // only include if your setup actually supports this arg
-  $args['starts_with'] = $char;
-}
-?>
-
-     <?php $my_query = null;
       $my_query = new WP_Query($args);
-      if( $my_query->have_posts() ) : ?>
-        <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-          <?php get_template_part('loops/people-post', get_post_format()); ?>
-        <?php endwhile;
-      endif; ?>
+
+      if ( $my_query->have_posts() ) :
+        while ( $my_query->have_posts() ) :
+          $my_query->the_post();
+
+          get_template_part('loops/people-post', get_post_format());
+
+        endwhile;
+      endif;
+
+      wp_reset_postdata();
+    ?>
   </div>
 
   <div class="row pagination-row justify-content-center">
-      <?php if ( function_exists('b4st_pagination') ) { b4st_pagination(); } else if ( is_paged() ) { ?>
-      <ul class="pagination">
-        <li class="page-item older">
-          <?php next_posts_link('<i class="fas fa-arrow-left"></i> ' . __('Previous', 'b4st')) ?></li>
-        <li class="page-item newer">
-          <?php previous_posts_link(__('Next', 'b4st') . ' <i class="fas fa-arrow-right"></i>') ?></li>
-      </ul>
-      <?php } ?>
-    </div>
-  </div>
+    <?php if ( $my_query->max_num_pages > 1 ) : ?>
+      <div class="pagination">
+        <?php
+          global $wp_query;
 
+          $orig_query = $wp_query;
+          $wp_query = $my_query;
 
+          $big = 999999999;
+
+          echo paginate_links(array(
+            'base'      => str_replace($big, '%#%', get_pagenum_link($big)),
+            'format'    => '?paged=%#%',
+            'current'   => max(1, get_query_var('paged')),
+            'next_text' => __('<i class="fas fa-angle-right"></i>'),
+            'prev_text' => __('<i class="fas fa-angle-left"></i>'),
+            'total'     => $my_query->max_num_pages,
+          ));
+
+          $wp_query = $orig_query;
+        ?>
+      </div>
+    <?php endif; ?>
   </div>
 </div>

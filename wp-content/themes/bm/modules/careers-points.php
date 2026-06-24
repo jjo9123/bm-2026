8a5617b6ae4896a65babe5e7feb823cff08b8c56
Joseph West <joe@incitenewbusiness.co.uk>
2026-06-24 10:47:58 +0100

    Remove images from bullets

--- wp-content/themes/bm/modules/careers-points.php
+++ wp-content/themes/bm/modules/careers-points.php
@@ -6,20 +6,20 @@
       <?php endif; ?>
     </div>
     <div class="col-sm-12 col-md-12">
-      <ul class="row">
-          <?php
-          if( have_rows('points') ):
-            while ( have_rows('points') ) : the_row(); ?>
-              <li class="col-sm-4" style="background-image: url('<?php the_sub_field('icon'); ?>');"><?php the_sub_field('txt'); ?></li>
-            <?php endwhile;
-          endif;
+      <ul class="styled">
+        <?php
+        if (have_rows('points')) :
+          while (have_rows('points')) : the_row(); ?>
+            <li><?php the_sub_field('txt'); ?></li>
+          <?php endwhile;
+        endif;
         ?>
       </ul>
     </div>
 
 
 <style>
-.clients li {
+/*.clients li {
   background-image: url(/wp-content/uploads/Careers/careers_benefits-icon1.png);
   background-repeat: no-repeat;
   background-position: left center;
@@ -27,7 +27,7 @@
   margin: 0;
   padding: 5px 15px 5px 35px;
   list-style: none;
-}
+}*/
 </style>
 
 

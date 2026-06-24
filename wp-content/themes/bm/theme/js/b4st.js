44bf8067c23a4321e9cea4d4fff733d0e782c9e9
Joseph West <joe@incitenewbusiness.co.uk>
2026-06-24 18:49:37 +0100

    Nav search fix

--- wp-content/themes/bm/theme/js/b4st.js
+++ wp-content/themes/bm/theme/js/b4st.js
@@ -116,43 +116,44 @@
     });
 
     /* Nav search toggle */
-    var c = $(".nav-search__toggle"),
-        d = $("#navSearchPanel");
+var c = $(".nav-search__toggle"),
+    d = $("#navSearchPanel");
 
-    function closeNavSearch() {
-      d.attr("hidden", true);
-      c.attr("aria-expanded", "false");
+function closeNavSearch() {
+  d.attr("hidden", "hidden").prop("hidden", true);
+  c.attr("aria-expanded", "false");
+}
+
+if (c.length && d.length) {
+  c.on("click", function (e) {
+    console.log("Search clicked");
+    e.preventDefault();
+
+    if (d.prop("hidden")) {
+      d.removeAttr("hidden").prop("hidden", false);
+      c.attr("aria-expanded", "true");
+      d.find("input, select, textarea, button").first().focus();
+    } else {
+      closeNavSearch();
     }
+  });
 
-    if (c.length && d.length) {
-
-      c.on("click", function (e) {
-        e.preventDefault();
-        if (d.is("[hidden]")) {
-          d.removeAttr("hidden");
-          c.attr("aria-expanded", "true");
-          d.find("input, select, textarea, button").first().focus();
-        } else {
-          closeNavSearch();
-        }
-      });
-
-      $(document).on("click", function (e) {
-        if (
-          !d.is("[hidden]") &&
-          !c.is(e.target) &&
-          !c.has(e.target).length &&
-          !d.is(e.target) &&
-          !d.has(e.target).length
-        ) {
-          closeNavSearch();
-        }
-      });
-
-      $(document).on("keydown", function (e) {
-        if (e.key === "Escape") closeNavSearch();
-      });
+  $(document).on("click", function (e) {
+    if (
+      !d.prop("hidden") &&
+      !c.is(e.target) &&
+      !c.has(e.target).length &&
+      !d.is(e.target) &&
+      !d.has(e.target).length
+    ) {
+      closeNavSearch();
     }
+  });
+
+  $(document).on("keydown", function (e) {
+    if (e.key === "Escape") closeNavSearch();
+  });
+}
 
     /* Gravity Forms textarea counter */
     function bindTextareaCounters() {

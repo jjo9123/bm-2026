426c5e873758fa31fcbbb73093d0ce2268898051
Joseph West <joe@incitenewbusiness.co.uk>
2026-08-20 12:28:41 +0100

    New form

--- wp-content/themes/bm/modules.php
+++ wp-content/themes/bm/modules.php
@@ -7,7 +7,7 @@ if( have_rows('options') ):
     elseif( get_row_layout() == 'stacked_form' ):
       get_template_part('modules/stacked-form');
 
-    elseif( get_row_layout() == 'form' ):
+    elseif( get_row_layout() == 'new_form' ):
       get_template_part('modules/new-form');
 
     elseif( get_row_layout() == 'faq_block' ):

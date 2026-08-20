8d9cdee57a78cf573cd8a9e39195bcf109378630
Joseph West <joe@incitenewbusiness.co.uk>
2026-08-20 12:12:41 +0100

    Form layout

--- wp-content/themes/bm/modules/new-form.php
+++ wp-content/themes/bm/modules/new-form.php
@@ -1,51 +1,54 @@
-<?php $bg_class = get_sub_field('bg_colour') ?: 'bm-pink';
-$anchor_link = get_sub_field('anchor_link'); ?>
+<?php
+$bg_class   = get_sub_field('bg_colour') ?: 'bm-pink';
+$anchor_link = get_sub_field('anchor_link');
+?>
 
 <section class="contact <?php echo esc_attr($bg_class); ?>">
   <div class="container">
     <div class="row"<?php if ($anchor_link) : ?> id="<?php echo esc_attr(sanitize_title($anchor_link)); ?>"<?php endif; ?>>
-      <div class="col-lg-12 mb-4">
+
+      <div class="col-12 mb-4">
         <h2 class="text-center">
           <?php the_sub_field('heading'); ?>
         </h2>
       </div>
 
-      
-        <?php
-          /*
-          if ( get_sub_field('txt_choice') == 'global' ): ?>
-            <p><?php the_field('txt', 'options'); ?></p>
-          <?php else: ?>
-            <p><?php the_sub_field('txt'); ?></p>
-          <?php endif;
-          */
-          ?>
-      
+      <!-- Text -->
+      <div class="col-12 col-lg-6 mb-4 mb-lg-0">
+        <div class="contact__text">
+          <?php the_sub_field('txt'); ?>
+        </div>
+      </div>
 
-      <div class="col-12 col-md-10 mx-auto">
-        <?php
+      <!-- Form -->
+      <div class="col-12 col-lg-6">
+        <div class="contact__form">
+          <?php
           $form_object = get_sub_field('form');
 
-          gravity_form_enqueue_scripts($form_object['id'], true);
+          if ($form_object && !empty($form_object['id'])) {
+              $form_id = (int) $form_object['id'];
 
-          // Set a global "current module form id" just for this render
-          $GLOBALS['bm_contact_footer_form_id'] = (int) $form_object['id'];
+              gravity_form_enqueue_scripts($form_id, true);
 
-          gravity_form(
-            $form_object['id'],
-            false,
-            false,
-            false,
-            null,   // no field_values needed
-            true,
-            1
-          );
+              $GLOBALS['bm_contact_footer_form_id'] = $form_id;
 
-          // Unset so it can't affect anything else later on the page
-          unset($GLOBALS['bm_contact_footer_form_id']);
+              gravity_form(
+                  $form_id,
+                  false,
+                  false,
+                  false,
+                  null,
+                  true,
+                  1
+              );
+
+              unset($GLOBALS['bm_contact_footer_form_id']);
+          }
           ?>
-
+        </div>
       </div>
+
     </div>
   </div>
 </section>
